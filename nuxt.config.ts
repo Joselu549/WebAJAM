5c4ede1648cac9932f1f28a8588c742cd46528df
José Luis Abellán Monreal <joseluisabellanmonreal@gmail.com>
2025-03-22 21:56:46 +0100

    Innitial commit angular

--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default {
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
-  tailwindcss: {
-    configPath: "tailwind.config.js", // Asegúrate de que este archivo exista.
-  },
-};
